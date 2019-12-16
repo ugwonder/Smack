4db8445cc2fbdcd6efb365a13cada474b10046a9
mgbachi_ugo <ugochukwu_mgbachi@yahoo.com>
2019-12-16 04:09:09 +0100

    lesson-02

--- app/src/main/java/com/mgbachi_ugo/smack/MainActivity.kt
+++ app/src/main/java/com/mgbachi_ugo/smack/MainActivity.kt
@@ -13,6 +13,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,4 +45,17 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+    fun loginBtnNavclicked(view: View) {
+
+    }
+
+    fun addChannelBtnNavclicked(view: View) {
+
+    }
+
+    fun sendMessageBtnClicked(view: View) {
+
+    }
+
+
 }
