abb60f10b8c59590e5abac315cc3f21cdc1e0ab4
mgbachi_ugo <ugochukwu_mgbachi@yahoo.com>
2019-12-17 12:47:47 +0100

    leson-05

--- app/src/main/java/com/mgbachi_ugo/smack/CreateUserActivity.kt
+++ app/src/main/java/com/mgbachi_ugo/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.mgbachi_ugo.smack
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvater = "profileDefault"
+    var avaterColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,10 +18,33 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvater(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avater = random.nextInt(28)
+        if (color == 0) {
+            userAvater = "light$avater"
+        } else{
+            userAvater = "dark$avater"
+        }
+        val resourceId = resources.getIdentifier(userAvater,"drawable", packageName)
+        createAvaterImageView.setImageResource(resourceId)
+
 
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvaterImageView.setBackgroundColor(Color.rgb(r,g,b))
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+        avaterColor = "[$savedB, $savedG, $savedR]"
+
+
 
     }
 
